5966328a3d3ed741be10204ed2df9b248af7f92b
Anay Wadhera <anay1018@gmail.com>
2022-12-25 17:02:13 +0600

    oplus: switch to AIDL Rust Powershare HAL
    Co-authored-by: Vaisakh Murali <mvaisakh@statixos.com>
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

---
+++ aidl/powershare/powershare.mk
@@ -0,0 +1,6 @@
+# Powershare
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += hardware/oplus/aidl/powershare/framework_compatibility_matrix.xml
+
+PRODUCT_PACKAGES += \
+    vendor.lineage.powershare-service.oplus
+
